8bedf49c645df1b436111a1026d7f8bcc4f0c3e6
Anurag Anand <anurag21j@gmail.com>
2018-01-13 11:51:12 +0530

    App Complete

--- app/src/main/java/com/example/anurag/diceapp/MainActivity.java
+++ app/src/main/java/com/example/anurag/diceapp/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.anurag.diceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = findViewById(R.id.rollButton);
+
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,
+                R.drawable.dice4,R.drawable.dice5,R.drawable.dice6};
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Dicee", "Button has been pressed");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+
+                int number2 = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee", "The random Number is "+number);
+
+                leftDice.setImageResource(diceArray[number]);
+
+                rightDice.setImageResource(diceArray[number2]);
+            }
+        });
     }
 }
